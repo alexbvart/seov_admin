45dfcb52481fa9749250ccac70543911ae55a281
Alexander Briones <alexanderbvart@gmail.com>
2021-11-13 08:28:47 -0500

    update service

--- services/put.js
+++ services/put.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-const put = async (src,id,pup) => {
-    const response = await axios.put(`${src}/${id}`,pup)
+const put = async ({src,id,put}) => {
+
+    const API_URL = process.env.NEXT_PUBLIC_SERVER_HOST
+    const response = await axios.put(`${API_URL}/${src}/${id}`, put)    
     return response
+
 }
 export default put;
 /* PUT requiere enviar una representación completa del recurso que se está modificando */
